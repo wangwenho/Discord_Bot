39823f6fb9518883dbc147edb6674373a473de4c
wangwenho <wangwenho@gmail.com>
2023-02-16 16:34:23 +0800

    專案

--- src/main.js
+++ src/main.js
@@ -5,9 +5,8 @@ dotenv.config()
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, c => {
+client.once(Events.ClientReady, (c) => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-// Log in to Discord with your client's token
 client.login(process.env.TOKEN);
