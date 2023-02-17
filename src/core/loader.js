b5e787a60b5bcef3050bf39bcc27f68aee35d7c0
wangwenho <wangwenho@gmail.com>
2023-02-17 21:21:38 +0800

    註冊指令成功

--- src/core/loader.js
+++ src/core/loader.js
@@ -1,19 +1,19 @@
 import { REST, Routes } from 'discord.js'
 import fg from 'fast-glob'
 
+
 const updateSlashCommands = async(commands) => {
   const rest =  new REST({version: 10}).setToken(process.env.TOKEN)
-
   const result = await rest.put(
     Routes.applicationGuildCommands(
       process.env.APPLICATION_ID,
-      '1075676351798988800',
+      '1075678016199479347',
     ),
     {
       body: commands,
     },
   )
-
+ 
   console.log(result)
 }
 
